9cb7071e42387b440fbe3b673121439f6dd38fa1
simon marchal <simon.marchal@epitech.eu>
2023-10-24 05:39:55 -0400

    update

--- ex_06/Gecko.java
+++ ex_06/Gecko.java
@@ -1,20 +1,4 @@
 public class Gecko {
-    /*
-    Copy your Gecko.java from the previous exercise and add an eat method which:
-3 takes a string as parameter ;
-3 is case insensitive ;
-3 returns nothing.
-Depending on the argument (value of the parameter), the Gecko must display:
-3 Yummy!, if the argument is equal to Meat ;
-3 Erk!, if the argument is equal to Vegetable ;
-3 I can't eat this!, otherwise.
-As usual, every sentence must be followed by a newline.
-Moreover, add an energy attribute to our Gecko:
-3 by default it is equal to 100 ;
-3 a gecko’s energy should always be between 0 and 100 (included).
-Add a setter (setEnergy) and a getter (getEnergy) for this attribute.
-Every time our Gecko eats something, it will win or lose some energy. If it eats
-     */
     private String name;
     private int age;
 
@@ -78,91 +62,21 @@ Every time our Gecko eats something, it will win or lose some energy. If it eats
     public void eat(String food) {
         if (food.equalsIgnoreCase("Meat")) {
             System.out.println("Yummy!");
+            this.energy += 10;
         } else if (food.equalsIgnoreCase("Vegetable")) {
             System.out.println("Erk!");
+            this.energy -= 10;
         } else {
             System.out.println("I can't eat this!");
+        }
+        if (this.energy > 100) {
+            this.energy = 100;
+        } else if (this.energy < 0) {
+            this.energy = 0;
         }
     }
 
     private int energy = 100;
-
-
-    public void work() {
-        if (this.energy >= 25) {
-            System.out.println("I'm working T.T");
-            this.energy -= 9;
-        } else {
-            System.out.println("Heyyy... I'm too sleepy, better take a nap!");
-            this.energy += 50;
-        }
-    }
-
-    public void fraternize(Gecko buddy) {
-        if (this.energy >= 30 && buddy.getEnergy() >= 30) {
-            System.out.println("Hello " + buddy.getName() + "!");
-            buddy.hello(this.name);
-            this.energy -= 9;
-            buddy.setEnergy(buddy.getEnergy() - 9);
-        } else {
-            System.out.println("...");
-        }
-    }
-
-    public void nap() {
-        System.out.println("zzzZZZzzzZZZzzz");
-        this.energy += 50;
-    }
-
-    public void hunt() {
-        if (this.energy >= 50) {
-            System.out.println("I'm going to hunt!");
-            this.energy -= 30;
-            int random = (int) (Math.random() * 3);
-            switch (random) {
-                case 0:
-                    System.out.println("I caught a lizard!");
-                    this.energy += 30;
-                    break;
-                case 1:
-                    System.out.println("I caught a butterfly!");
-                    this.energy += 10;
-                    break;
-                case 2:
-                    System.out.println("I caught a spider!");
-                    this.energy += 25;
-                    break;
-            }
-        } else {
-            System.out.println("I'm too hungry to hunt...");
-        }
-    }
-
-    public void fight(Gecko opponent, String weapon) {
-        if (this.energy >= 20 && opponent.getEnergy() >= 20) {
-            System.out.println("I'm going to fight " + opponent.getName() + " using " + weapon + "!");
-            this.energy -= 20;
-            opponent.setEnergy(opponent.getEnergy() - 20);
-            int random = (int) (Math.random() * 3);
-            switch (random) {
-                case 0:
-                    System.out.println("I won!");
-                    this.energy += 20;
-                    break;
-                case 1:
-                    System.out.println("I lost...");
-                    opponent.setEnergy(opponent.getEnergy() + 20);
-                    break;
-                case 2:
-                    System.out.println("I'm too tired to fight...");
-                    this.energy += 10;
-                    opponent.setEnergy(opponent.getEnergy() + 10);
-                    break;
-            }
-        } else {
-            System.out.println("I'm too tired to fight...");
-        }
-    }
 
     public void setEnergy(int energy) {
         this.energy = energy;
@@ -187,7 +101,4 @@ Every time our Gecko eats something, it will win or lose some energy. If it eats
     public String getName() {
         return this.name;
     }
-
-
-
 }
